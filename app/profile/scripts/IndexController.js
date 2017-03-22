2b205fcc4612cfb900a9a3ab1cf85bd64ec88c0c
Kim Lum <kimberly.j.lum@gmail.com>
2017-03-22 13:56:36 -0700

    added menu items

--- app/profile/scripts/IndexController.js
+++ app/profile/scripts/IndexController.js
@@ -1,5 +1,9 @@
 angular
-  .module('profile')
-  .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
+    .module('profile')
+    .controller('IndexController', function($scope, supersonic) {
+        // Controller functionality here
+        $scope.showMenu = false;
+        $scope.toggleMenu = function() {
+            $scope.showMenu = !$scope.showMenu;
+        };
   });
