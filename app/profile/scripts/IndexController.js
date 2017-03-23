7cf3627188b9a7c81ec03b807fd10c85c159816d
Kim Lum <kimberly.j.lum@gmail.com>
2017-03-23 12:18:43 -0700

    started adding history view

--- app/profile/scripts/IndexController.js
+++ app/profile/scripts/IndexController.js
@@ -6,4 +6,18 @@ angular
         $scope.toggleMenu = function() {
             $scope.showMenu = !$scope.showMenu;
         };
+
+        $scope.showHistory = function() {
+            $scope.showMenu = false;
+            var view = new supersonic.ui.View("profile#history");
+            supersonic.ui.layers.push(view);
+
+            // supersonic.ui.views.find("settingsView").then( function(startedView) {
+            //   supersonic.ui.layers.push(startedView);
+            // });
+
+            // // Push with custom animation
+            // var customAnimation = supersonic.ui.animate("flipVerticalFromTop");
+            // supersonic.ui.layers.push(view, { animation: customAnimation });
+        };
   });
