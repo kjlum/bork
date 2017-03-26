3d005014a09c80bdff671f30c238657991cd564d
Kim Lum <kimberly.j.lum@gmail.com>
2017-03-25 20:16:37 -0700

    started adding new user screen

--- app/profile/scripts/IndexController.js
+++ app/profile/scripts/IndexController.js
@@ -2,6 +2,19 @@ angular
     .module('profile', ['common', 'ngAnimate'])
     .controller('IndexController', function($scope, supersonic) {
         // Controller functionality here
+        var newUser = localStorage.getItem('isNewUser');
+        var puppies = localStorage.getItem('puppies');
+        if(newUser == null) {
+            supersonic.logger.log("heyo");
+            // new user, show welcome screen
+            localStorage.setItem('isNewUser', JSON.stringify("true"));
+            var view = new supersonic.ui.View("profile#initial");
+            supersonic.ui.layers.push(view);
+        } else {
+
+        }
+        
+
         $scope.showMenu = false;
         $scope.toggleMenu = function() {
             $scope.showMenu = !$scope.showMenu;
@@ -17,5 +30,21 @@ angular
             $scope.showMenu = false;
             var view = new supersonic.ui.View("profile#potty");
             supersonic.ui.layers.push(view);
+        };
+
+        $scope.clear = function() {
+            localStorage.clear();
+        };
+
+        $scope.newProfile = function() {
+            var view = new supersonic.ui.View("profile#new");
+            supersonic.ui.layers.push(view);
+        };
+
+        $scope.finishProfile = function() {
+            if(puppies == null) {
+                // TODO: first puppy
+
+            }
         }
   });
